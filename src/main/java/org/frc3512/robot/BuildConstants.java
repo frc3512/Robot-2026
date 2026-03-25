17bc2724f2b504a81573ca5b6aebbb0d176cb2ab
Codi56 <codi.samfong@gmail.com>
2026-03-24 20:33:18 -0700

    Mirrored Nz auto and modifyed Hp auton(needs testing)

--- src/main/java/org/frc3512/robot/BuildConstants.java
+++ src/main/java/org/frc3512/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "28984f2bd3ce5fe34ae979fc148a8d51527c63d2";
-  public static final String GIT_DATE = "03-25-2026 00:53:26 GMT";
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "6e1e38d68ceff6bdb8d624fd23c1eab6b473d472";
+  public static final String GIT_DATE = "03-25-2026 01:33:12 GMT";
   public static final String GIT_BRANCH = "Cerberus";
-  public static final String BUILD_DATE = "03-25-2026 01:26:52 GMT";
-  public static final long BUILD_UNIX_TIME = 1774402012909L;
+  public static final String BUILD_DATE = "03-25-2026 03:16:40 GMT";
+  public static final long BUILD_UNIX_TIME = 1774408600890L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
